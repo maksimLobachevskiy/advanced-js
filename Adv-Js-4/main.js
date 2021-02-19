81482987acfdeed938bb60353e13024f8f74e459
Maksim Lobachevskiy <lobachevskiy.maksim@gmail.com>
2021-02-19 20:09:49 +0200

    Adv-JS-4

---
+++ Adv-Js-4/main.js
@@ -0,0 +1,48 @@
+function fetchFilms(url) {
+    fetch(url).then((res) => {
+        return res.json();
+    }).then((data) => {
+        const characters = getActors(data);
+        displayData(data);
+        const loadChars = document.querySelectorAll(".chars");
+        characters.forEach(displayCharacters(loadChars));
+    })
+
+    function getActors({results}) {
+        return results.map(({characters}) => {
+            return characters;
+        });
+    }
+}
+
+function displayData(data) {
+    let output = '';
+    data.results.forEach(item => {
+        output += `<div>Film ID: ${item["episode_id"]}</div><h3>${item.title}</h3>
+                    <p><strong>Short description:</strong> ${item["opening_crawl"]}</p><h3>Characters:</h3><ul class="chars"></ul>`
+
+    })
+    document.body.innerHTML = output;
+}
+
+function displayCharacters(loadChars) {
+    return function (charList, filmId) {
+        loadChars[filmId].innerHTML = "<div class=\"lds-roller\"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>";
+        const fetchChars = charList.map(charURL => {
+            return fetch(charURL).then((res) => {
+                return res.json();
+            })
+        });
+        Promise.all(fetchChars).then((data) => {
+            loadChars[filmId].innerHTML = "";
+            const charNames = data.map(({name}) => name);
+            charNames.forEach(item => {
+                const li = document.createElement("li");
+                li.innerText = item;
+                loadChars[filmId].append(li);
+            });
+        });
+    }
+}
+
+fetchFilms('https://swapi.dev/api/films/');
